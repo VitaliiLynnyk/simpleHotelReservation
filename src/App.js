af909bbf492fb95b8e952ad91fc42a50c3122666
VitaliiLynnyk <linnik2101@gmail.com>
2020-05-05 22:58:36 +0200

    add simple routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Home, Rooms, Error, SingleRoom } from './pages';
 
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
