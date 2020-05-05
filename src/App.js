cef394885390a771b37f354d4842062f6a609c44
VitaliiLynnyk <linnik2101@gmail.com>
2020-05-05 23:04:51 +0200

    add initial navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import { Navbar } from './components';
 import { Home, Rooms, Error, SingleRoom } from './pages';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { FaAlignRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import logo from '../images/logo.svg';
+
+export default class Navbar extends Component {
+  render() {
+    return <div>Navbar</div>;
+  }
+}
